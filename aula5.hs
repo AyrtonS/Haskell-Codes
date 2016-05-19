e567f4d0d9dd598e2f2f7388fb3197827f10a833
Ayrton Sousa Marinho <ayrtontisolution@hotmail.com.br>
2016-05-19 19:47:30 -0300

    Update aula5.hs

--- aula5.hs
+++ aula5.hs
@@ -17,3 +17,16 @@ f2 (x,y) = y
 
         f3_2 ::  Num a => [a] -> [a]
         f3_2 ( x : y) = y
+
+fun :: String
+fun =
+      case [10,20,30,40] of
+      [] -> "Lista Vazia"
+      x:xs -> "cabeca: "++ show x ++ " cauda: "++ show xs
+
+
+fun2 :: Integer
+fun2 =
+  case [10..20] of
+  x:y:z:_ -> x + y + z
+  _ -> 0
