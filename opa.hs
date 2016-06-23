b2c5e53b3c56b5e2025db3f80839fcd690321740
Ayrton Sousa Marinho <ayrtontisolution@hotmail.com.br>
2016-06-23 18:48:52 -0300

    Update opa.hs

--- opa.hs
+++ opa.hs
@@ -1,5 +1,7 @@
 module Main(main) where
 import System.Environment (getArgs,getProgName)
+import System.Exit (exitFailure)
+
 -- getArgs :: IO[String]
 
 main = do
@@ -9,3 +11,19 @@ main = do
   args <- getArgs
   putStr "The arguments are..."
   print args
+
+
+-------------------------------------------------------------------
+
+
+main = do
+  args <- getArgs
+  case args og
+    [input,output] ->
+      do putStrLn ("Entrada: "++ input)
+         putStrLn ("saida : "++ output)
+    _ ->
+      do progName <- getProgName
+         putStrLn ("Chamada invalida do programa "++ progName)
+         putStrLn ("Uso: "++progName++" <arquivo-entrada> <arquivo-saida>")
+         exitFailure
